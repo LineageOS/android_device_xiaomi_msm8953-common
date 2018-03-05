ff809becd7264718eb1fb2d5064532cb376062a6
deadman96385 <seanhoyt963@gmail.com>
2018-03-05 16:55:42 +0000

    msm8953-common: Enable Lineage Hardware
    Looks like this got lost in making the common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,6 +157,10 @@ TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += \
+    $(VENDOR_PATH)/lineagehw
+
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
