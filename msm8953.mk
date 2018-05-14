03b5fa14778e4c096f7fc74aedf14e5370ddb15d
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    msm8953-common: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I28d354ea59aa11ea368f4785691eecb83d401828
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -87,8 +87,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle \
-    tinymix
+    libqcompostprocbundle 
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
