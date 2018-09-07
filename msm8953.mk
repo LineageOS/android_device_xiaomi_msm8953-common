46f0d16dde996c6c7a7becf79bd124b5739986df
LuK1337 <priv.luk@gmail.com>
2018-09-07 15:21:20 +0200

    msm8953-common: Drop libbt-vendor
    * With QTI bluetooth it's no longer needed.
    
    Change-Id: Iba4b0b6ba24fe1b85e7175291ed84769615ffadd

--- msm8953.mk
+++ msm8953.mk
@@ -114,10 +114,6 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
