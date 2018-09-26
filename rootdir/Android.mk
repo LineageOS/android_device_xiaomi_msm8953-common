c6a1c46a8ad0d31c7f3844432508b9dca9ab862d
Bruno Martins <bgcngm@gmail.com>
2018-09-26 09:54:39 +0000

    msm8953-common: Move offmode charging daemon away
     * Follow-up change for commit 75814d2 that simply moves the
       prebuilt to its proper place.
    
    Change-Id: I70d9e5c480dc8a421f9c9ba073498eae5b52463d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -92,13 +92,3 @@ LOCAL_SRC_FILES    := etc/move_wifi_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-# Offmode charging
-include $(CLEAR_VARS)
-LOCAL_MODULE          := chargeonlymode
-LOCAL_MODULE_OWNER    := xiaomi
-LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES       := sbin/chargeonlymode
-LOCAL_MODULE_TAGS     := optional
-LOCAL_MODULE_CLASS    := EXECUTABLES
-include $(BUILD_PREBUILT)
