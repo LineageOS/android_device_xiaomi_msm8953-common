b13c7a2e435228d057baa222b1c36fab1fec1b03
Soumil Shah <soumil_92@codeaurora.org>
2018-10-05 01:41:03 -0700

    msm8953-common: Fix build error
    Disallow board to turn overriding rule command into an error
    
    Change-Id: I2a788ea4c50af0cb56ecf9de9b979e88f8e103a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,9 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+# Build
+BUILD_BROKEN_DUP_RULES := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true
