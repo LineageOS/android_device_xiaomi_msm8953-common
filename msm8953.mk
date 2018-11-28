00bcf7dbaab439a27cb4ddf443ac45d49036954a
Artem Borisov <dedsa2002@gmail.com>
2018-11-28 16:22:33 +0400

    msm8953-common: Update ramdisk to P
    * From LA.UM.7.6.r1-03600-89xx.0
    
    Change-Id: I125d09509bf72c8ec06a401d690e32b0c286f37c

--- msm8953.mk
+++ msm8953.mk
@@ -291,7 +291,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
-    init.qcom.post_boot.sh
+    init.qcom.post_boot.sh \
+    move_time_data.sh \
+    move_wifi_data.sh
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
