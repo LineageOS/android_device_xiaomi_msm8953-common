bbb85a7c7040a75ee9293aec3f71fe40e4340fbd
Artem Borisov <dedsa2002@gmail.com>
2018-11-28 16:22:33 +0400

    msm8953-common: Update ramdisk to P
    * From LA.UM.7.6.r1-03600-89xx.0
    
    Change-Id: I125d09509bf72c8ec06a401d690e32b0c286f37c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -74,6 +74,22 @@ LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE	   := move_time_data.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/move_time_data.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := move_wifi_data.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/move_wifi_data.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # Offmode charging
 include $(CLEAR_VARS)
 LOCAL_MODULE          := chargeonlymode
