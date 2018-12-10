90f3f4cca9de273a5eabd00fcd7922fb45d39ff8
Ethan Chen <intervigil@gmail.com>
2018-12-09 16:42:26 -0800

    msm8953-common: Use local DATA_IPACFG_MGR
    Change-Id: I6b074707510bde8fb001b42dbbe1654c6ebdb6ad

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,6 +165,9 @@ TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
+# IPA
+USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
+
 # Lineage Hardware
 JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(VENDOR_PATH)/lineagehw|**/*.java
 
