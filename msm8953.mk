93503eafe4ec8d66b9705eb93c39edc2ca10a919
elektroschmock <elektroschmock78@googlemail.com>
2018-12-18 14:28:20 +0100

    msm8953-common: Don't explicitly build configstore@1.0-service
    Configstore's build rule is already included by
    build/make/target/product/base_vendor.mk
    
    Change-Id: Ib73d27f0e19f1eda9a50cd305b323250e122ef87

--- msm8953.mk
+++ msm8953.mk
@@ -124,10 +124,6 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     Snap
 
-# Configstore
-PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
