bd965effa4f8819681de9cb2233eb839ad4a7745
Albert I <krascgq@outlook.co.id>
2019-01-05 19:49:23 +0800

    msm8953-common: Make tree compatible with both 3.18 and 4.9 kernels
    * Restored 3.18's legacy /devices/soc path in both fstab variants.
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Change-Id: Ia696fc239d3e66d3209a2a597cd7ac5f74302c9c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,13 +40,15 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+ifeq ($(TARGET_KERNEL_VERSION),4.9)
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_VERSION := 4.9
+endif
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
