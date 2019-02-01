5707c79af1a2ba7f6da9db8595ea34809f3d3b6b
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-02-01 15:12:22 +0700

    msm8953-common: import qcom power hal
    * Plain QCOM power HAL based on tag LA.UM.7.6.r1-04000-89xx.0
    
    Signed-off-by: KenHV <kenhv@null.net>
    Change-Id: I9b31871af4fbbda285d92dc5bacca6a522d57d2b

---
+++ power/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
+LOCAL_C_INCLUDES := external/libxml2/include \
+                     external/icu/icu4c/source/common
+
+LOCAL_SRC_FILES += power-8953.c
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+LOCAL_MODULE := power.qcom
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
