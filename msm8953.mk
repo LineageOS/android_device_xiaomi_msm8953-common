77cc799cba42838dc872c6a0487bf36df0ef43a3
Artem Borisov <dedsa2002@gmail.com>
2019-02-17 23:08:33 +0530

    msm8953-common: Add QC RIL service support
    * Remove android.hardware.radio.deprecated from manifest
    * Pin modified qcrild.rc
    
    Change-Id: I79e1242fe1de021905caddf5ed1085b2ec0c1146

--- msm8953.mk
+++ msm8953.mk
@@ -302,6 +302,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
