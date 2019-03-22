8b14732a24869b9b74e72788aba0d2c61a51c7df
LuK1337 <priv.luk@gmail.com>
2019-03-22 03:15:36 +0100

    msm8953-common: Don't set custom file attributes for cnss-daemon
    * CAF only sets them for /system/bin/cnss-daemon which doesn't exist.
    * Fixes: cannot execve('/vendor/bin/cnss-daemon'): Permission denied
    
    Change-Id: I74dd2fc53f5e0c5f5658328aac21ca14c6fe0828

--- config.fs
+++ config.fs
@@ -40,12 +40,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
 
-[system/bin/cnss-daemon]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: NET_BIND_SERVICE
-
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
