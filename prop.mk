c7e982a0fc0a264b0753a135a9597b78e2e9f760
Artem Borisov <dedsa2002@gmail.com>
2019-04-05 19:52:44 +0530

    msm8953-common: Re-enable some UX optimizations from stock
    * These were removed in 0570b23 and 3d4186f as I blamed them
      for UI performance degradation.
    * Further testing shows that they actually reduce jank in some cases.
    
    Change-Id: I6f3ae240366cc49e1a25d2f76ddd847ced0fb8f4

--- prop.mk
+++ prop.mk
@@ -75,6 +75,7 @@ debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
+debug.sf.latch_unsignaled=1 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.debug.wfd.enable=1 \
@@ -191,6 +192,10 @@ persist.timed.enable=true
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
 net.tcp.2g_init_rwnd=10
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
