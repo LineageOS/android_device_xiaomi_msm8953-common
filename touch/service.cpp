f91bcc36e030e1902816e5042800d696c732f0e9
Artem Borisov <dedsa2002@gmail.com>
2019-04-05 16:06:45 +0530

    msm8953-common: touch: Fix wrong log tag
    Change-Id: Ic672a2870b9820875588db78084e603247395942

--- touch/service.cpp
+++ touch/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.touch@1.0-service.xiaomi_8996"
+#define LOG_TAG "vendor.lineage.touch@1.0-service.xiaomi_8953"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
