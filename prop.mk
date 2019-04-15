5149d2e6c4e472004b9ec712e449c362f5e0bc96
Artem Borisov <dedsa2002@gmail.com>
2019-04-15 22:43:40 +0530

    msm8953-common: Update properties
    * Add vendor prefix to a few more props
    * Remove useless props
    * Don't explicitly disable Wi-Fi Calling
    
    Change-Id: I6ce9d0f01c0e1bdcd6d21fd767d822b5c46392e0

--- prop.mk
+++ prop.mk
@@ -60,7 +60,7 @@ vidc.enc.dcvs.extra-buff-count=2
 
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.cne.feature=1 \
+persist.vendor.cne.feature=1 \
 persist.dpm.feature=1
 
 # Coresight
@@ -132,11 +132,10 @@ vendor.video.disable.ubwc=1
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.sys.fw.bservice_enable=true
+ro.vendor.qti.sys.fw.bservice_enable=true
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.fw.dex2oat_thread_count=4 \
 ro.vendor.extension_library=libqti-perfd-client.so
 
 # Netmgrd
@@ -160,20 +159,11 @@ persist.rild.nitz_short_ons_3=""
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PROVISIONED=1 \
-persist.data.iwlan.enable=true \
-persist.dbg.ims_volte_enable=1 \
 persist.dbg.volte_avail_ovr=1 \
 persist.dbg.vt_avail_ovr=1 \
-persist.dbg.wfc_avail_ovr=0 \
+persist.dbg.wfc_avail_ovr=1 \
 persist.vendor.radio.apm_sim_not_pwdn=1 \
-persist.radio.calls.on.ims=0 \
-persist.radio.csvt.enabled=false \
-persist.radio.hw_mbn_update=0 \
-persist.radio.jbims=0 \
-persist.radio.mt_sms_ack=20 \
 persist.radio.multisim.config=dsds \
-persist.radio.sw_mbn_update=0 \
-persist.radio.videopause.mode=1 \
 persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
@@ -182,8 +172,7 @@ rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
-service.qti.ims.enabled=1 \
-telephony.lteOnCdmaDevice=1
+service.qti.ims.enabled=1
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -199,7 +188,7 @@ sys.use_fifo_ui=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.sys.usb.config.extra=none
+persist.vendor.sys.usb.config.extra=none
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
