64c79571c4a0267bb0032bb7b8fe6d9f144117d8
Albert I <kras@raphielgang.org>
2019-06-13 20:38:41 +0800

    msm8953-common: Nuke data migration scripts
    * These are intended for O -> P upgrade which isn't the case for us.
    * Remove them to avoid unnecessary SELinux denials, especially time data.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I91a3da446c98a5e638aaaa9ea0788e48aafea264

--- msm8953.mk
+++ msm8953.mk
@@ -288,9 +288,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.bt.sh \
-    init.qcom.post_boot.sh \
-    move_time_data.sh \
-    move_wifi_data.sh
+    init.qcom.post_boot.sh
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -74,19 +74,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE	   := move_time_data.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/move_time_data.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := move_wifi_data.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/move_wifi_data.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
-include $(BUILD_PREBUILT)
