b476e281dcc9bfbd9240aaf9d4ba1d86f22ba4e8
Artem Borisov <dedsa2002@gmail.com>
2019-07-22 19:17:01 +0530

    Revert "Revert "msm8953-common: Update ZRAM configuration""
    * 512M disk is just too small for us and gets overfilled pretty quickly.
    
    This reverts commit f5c4392293959e4ffc69bdb112798d7886645b75.
    
    Change-Id: I4bddc3fb35f81f2dce441cb269417c60c2b03956

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -423,7 +423,11 @@ else
     fi
 
     # Set allocstall_threshold to 0 for all targets.
+    # Set swappiness to 100 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
+    echo 100 > /proc/sys/vm/swappiness
+
+    configure_zram_parameters
 
     configure_read_ahead_kb_values
 
