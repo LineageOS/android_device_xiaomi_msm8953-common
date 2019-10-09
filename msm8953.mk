3cc9f9e9eae75f33b942883e2051fb77a04fe7ca
dianlujitao <dianlujitao@lineageos.org>
2019-10-09 19:54:50 +0800

    msm8953-common: Build new GNSS HAL
     * Remove unnecessary dependencies on liblbs_core.so
     * Hardcode vendor loc HIDL version to 3.0 to enable vendor HIDL when
       building w/o BSP.
    
    Change-Id: I01f661f1d1e038737801f5a9db88e3388d470f74

--- msm8953.mk
+++ msm8953.mk
@@ -167,11 +167,25 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
-# GNSS
+# GPS / Location
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.2.vendor \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss@3.0.vendor
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libwifi-hal-ctrl
+
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # Health
 PRODUCT_PACKAGES += \
