c4daaeccaeea93d3e9b64fba7e7c599cdde9bdee
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 08:15:55 -0700

    msm8953-common: Use component-override to configure crosshatch Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on crosshatch devices.
    
    Remove usages of profile_supported_sap from overlays as they no
    longer configure the enable state of .sap.SapService.
    
    Bug: 135048762
    Test: adb shell pm query-services \
          com.android.bluetooth/.hfpclient.HeadsetClientService
    
    Change-Id: I89ba2aa61492f65b269849597282594507d2fdc3

--- msm8953.mk
+++ msm8953.mk
@@ -113,6 +113,10 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
