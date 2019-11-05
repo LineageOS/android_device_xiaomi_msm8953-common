62b0e1b299e0abf74c1038c45827f25f89943e07
Aryan <arykedare@gmail.com>
2019-11-05 21:20:04 +0530

    msm8953-common: Nuke Kernel Path
    * define in device specific trees
    
    Change-Id: Ia30780fb519ab602af427dc16ba36df12f195dc9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,11 +41,11 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 ifeq ($(TARGET_KERNEL_VERSION),4.9)
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 TARGET_KERNEL_CLANG_COMPILE := true
 endif
+
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
