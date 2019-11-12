bee69ddb5a6c4f281e937817e3ef53a8d54358e7
Quallenauge <Hamsi2k@freenet.de>
2019-11-12 22:49:17 +0100

    msm8953-common: Exclude Snap overlays from RRO
    * Snap overlays don't work with RRO
    
    Change-Id: I781ce4c723cd69adb2cf568c3a31b385468444e8

--- msm8953.mk
+++ msm8953.mk
@@ -22,7 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
