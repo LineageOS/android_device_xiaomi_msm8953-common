5206c0e7f038b897c3d481a0dd86d2769bd81ec0
ShihabZzz <Shihab.riadn@gmail.com>
2020-01-03 19:05:00 +0600

    msm8953-common: Refactor and fix WCNSS symlinks
    [SebaUbuntu]: Move WCNSS_cfg.dat to vendor
    
    Change-Id: Ib96f31e876b4cbf3e403d58847cbd77c52bea84e

--- msm8953.mk
+++ msm8953.mk
@@ -380,7 +380,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
