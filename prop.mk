11b1cf65308acf22d8a8b3c348a651fda695bc11
root2raj <ruturaj1999@gmail.com>
2020-02-11 20:15:23 +0530

    msm8953-common: Enable HAL3
    Change-Id: I91dfa3439f637e608726fb06f8ac9604876238d1

--- prop.mk
+++ prop.mk
@@ -61,7 +61,8 @@ persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
-vidc.enc.dcvs.extra-buff-count=2
+vidc.enc.dcvs.extra-buff-count=2 \
+persist.camera.HAL3.enabled=1
 
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
