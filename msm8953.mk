8933d79be8d2098e7d2a4e58f47127c4c07c8cdc
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:39:35 -0800

    msm8953-common: Remove unnecessary android.hardware.radio entries
    * These are implicitly included by AOSP.
    
    Change-Id: I9e557444161e7ca60793bddde82330a6bd04908b

--- msm8953.mk
+++ msm8953.mk
@@ -312,12 +312,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
-    android.hardware.radio@1.3 \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
-    android.hardware.radio.config@1.1 \
-    android.hardware.radio.config@1.2 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libcnefeatureconfig \
