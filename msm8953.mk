c0de680cf152201a1653b4510c5fd330278e82d1
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:24:39 -0800

    msm8953-common: Remove bogus ebtables/ethertypes/libebtc packages
    * These don't exist.
    
    Change-Id: I08f319e26867eb0b8fcdfaa632b4f6061f33b982

--- msm8953.mk
+++ msm8953.mk
@@ -156,12 +156,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
