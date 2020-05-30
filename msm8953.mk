d35a141c5d5a3dab0216c8ee2163288a97437840
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-05-30 15:12:57 +0530

    msm8953-common: build libqti_vndfwk_detect package
    required to fix [dlopen failed: library "libqti_vndfwk_detect.so" not found]
    
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>
    Change-Id: Ic0bbb5f5ce539086285f9273c44b143c4ae6b3be

--- msm8953.mk
+++ msm8953.mk
@@ -270,7 +270,9 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
