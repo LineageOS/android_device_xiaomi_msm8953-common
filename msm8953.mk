e3e203d643a9124f3b43a6c982eb2248935f184c
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-05-30 18:03:00 +0530

    msm8953-common: drop liblight from common tree
    liblight causing no notification led while charging
    
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>
    Change-Id: Ib36eb03293d87ede66f9b2317148f02522d89861

--- msm8953.mk
+++ msm8953.mk
@@ -222,12 +222,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
