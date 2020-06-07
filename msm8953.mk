5287994e8e566e3d3babe84b1d8d0ab5b4297473
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-06-07 21:33:10 +0530

    msm8953-common: fix spaces
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>
    Change-Id: Ifca0c357d40be1f5a0606a91497e82958e804fe0

--- msm8953.mk
+++ msm8953.mk
@@ -86,21 +86,21 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
-	$(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
-	$(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-	$(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-	$(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
-	$(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
+    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
 
 # XML Audio configuration files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Camera
 PRODUCT_PACKAGES += \
--- prop.mk
+++ prop.mk
@@ -4,240 +4,240 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-af.fast_track_multiplier=2 \
-audio.deep_buffer.media=true \
-audio.offload.disable=true \
-audio.offload.min.duration.secs=30 \
-audio.offload.video=true \
-ro.config.media_vol_steps=25 \
-ro.config.vc_call_vol_steps=7 \
-ro.af.client_heap_size_kbyte=7168 \
-ro.vendor.audio.sdk.fluencetype=fluence \
-ro.vendor.audio.sdk.ssr=false \
-persist.vendor.btstack.enable.splita2dp=false \
-persist.audio.dirac.speaker=true \
-persist.vendor.audio.fluence.speaker=true \
-persist.vendor.audio.fluence.voicecall=true \
-persist.vendor.audio.fluence.voicerec=true \
-hpx_send_params=1 \
-vendor.audio.use.dts_eagle=true \
-use.dts_eagle=true \
-vendor.audio.flac.sw.decoder.24bit=true \
-vendor.audio.offload.buffer.size.kb=64 \
-vendor.audio.offload.gapless.enabled=true \
-vendor.audio.offload.multiaac.enable=true \
-vendor.audio.offload.multiple.enabled=false \
-vendor.audio.offload.track.enable=true \
-vendor.audio.parser.ip.buffer.size=0 \
-vendor.audio.playback.mch.downsample=true \
-vendor.audio.pp.asphere.enabled=false \
-vendor.audio.safx.pbe.enabled=true \
-vendor.audio.tunnel.encode=false \
-vendor.audio.use.sw.alac.decoder=true \
-vendor.audio.use.sw.ape.decoder=true \
-vendor.audio_hal.period_size=192 \
-vendor.voice.conc.fallbackpath=deep-buffer \
-vendor.voice.path.for.pcm.voip=true \
-vendor.voice.playback.conc.disabled=true \
-vendor.voice.record.conc.disabled=false \
-vendor.voice.voip.conc.disabled=true
+    af.fast_track_multiplier=2 \
+    audio.deep_buffer.media=true \
+    audio.offload.disable=true \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7 \
+    ro.af.client_heap_size_kbyte=7168 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    persist.vendor.btstack.enable.splita2dp=false \
+    persist.audio.dirac.speaker=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=true \
+    hpx_send_params=1 \
+    vendor.audio.use.dts_eagle=true \
+    use.dts_eagle=true \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=0 \
+    vendor.audio.playback.mch.downsample=true \
+    vendor.audio.pp.asphere.enabled=false \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio_hal.period_size=192 \
+    vendor.voice.conc.fallbackpath=deep-buffer \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.voice.playback.conc.disabled=true \
+    vendor.voice.record.conc.disabled=false \
+    vendor.voice.voip.conc.disabled=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-bluetooth.hfp.client=1 \
-vendor.qcom.bluetooth.soc=smd \
-vendor.bluetooth.soc=smd \
-ro.bluetooth.hfp.ver=1.7 \
-ro.bluetooth.dun=true \
-ro.bluetooth.sap=true \
-ro.qualcomm.bluetooth.ftp=true \
-ro.qualcomm.bluetooth.hfp=true \
-ro.qualcomm.bluetooth.hsp=true \
-ro.qualcomm.bluetooth.map=true \
-ro.qualcomm.bluetooth.nap=true \
-ro.qualcomm.bluetooth.opp=true \
-ro.qualcomm.bluetooth.pbap=true \
-ro.qualcomm.bt.hci_transport=smd
+    bluetooth.hfp.client=1 \
+    vendor.qcom.bluetooth.soc=smd \
+    vendor.bluetooth.soc=smd \
+    ro.bluetooth.hfp.ver=1.7 \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.sap=true \
+    ro.qualcomm.bluetooth.ftp=true \
+    ro.qualcomm.bluetooth.hfp=true \
+    ro.qualcomm.bluetooth.hsp=true \
+    ro.qualcomm.bluetooth.map=true \
+    ro.qualcomm.bluetooth.nap=true \
+    ro.qualcomm.bluetooth.opp=true \
+    ro.qualcomm.bluetooth.pbap=true \
+    ro.qualcomm.bt.hci_transport=smd
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-camera.display.lmax=1280x720 \
-camera.display.umax=1920x1080 \
-vendor.camera.aux.packagelist=org.lineageos.snap,com.google.android.GoogleCamera \
-vendor.camera.hal1.packagelist=com.whatsapp,com.facebook.katana,com.instagram.android,com.snapchat.android \
-camera.lowpower.record.enable=1 \
-media.camera.ts.monotonic=1 \
-persist.camera.gyro.disable=0 \
-persist.camera.isp.clock.optmz=0 \
-persist.camera.stats.test=5 \
-persist.vendor.qti.telephony.vt_cam_interface=1 \
-vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1 \
-debug.sf.enable_gl_backpressure=1
+    camera.display.lmax=1280x720 \
+    camera.display.umax=1920x1080 \
+    vendor.camera.aux.packagelist=org.lineageos.snap,com.google.android.GoogleCamera \
+    vendor.camera.hal1.packagelist=com.whatsapp,com.facebook.katana,com.instagram.android,com.snapchat.android \
+    camera.lowpower.record.enable=1 \
+    media.camera.ts.monotonic=1 \
+    persist.camera.gyro.disable=0 \
+    persist.camera.isp.clock.optmz=0 \
+    persist.camera.stats.test=5 \
+    persist.vendor.qti.telephony.vt_cam_interface=1 \
+    vidc.enc.dcvs.extra-buff-count=2 \
+    persist.camera.HAL3.enabled=1 \
+    debug.sf.enable_gl_backpressure=1
 
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.cne.feature=1 \
-persist.dpm.feature=1
+    persist.vendor.cne.feature=1 \
+    persist.dpm.feature=1
 
 # Coresight
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.debug.coresight.config=stm-events
+    persist.debug.coresight.config=stm-events
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.egl.hw=0 \
-debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
-debug.mdpcomp.logs=0 \
-debug.sf.enable_hwc_vds=1 \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
-debug.sf.recomputecrop=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.demo.hdmirotationlock=false \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
-ro.opengles.version=196610 \
-ro.qualcomm.cabl=0 \
-ro.vendor.display.cabl=2 \
-sdm.debug.disable_skip_validate=1 \
-vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1
+    debug.egl.hw=0 \
+    debug.enable.sglscale=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.recomputecrop=0 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.hwc.mdpcomp.enable=true \
+    ro.opengles.version=196610 \
+    ro.qualcomm.cabl=0 \
+    ro.vendor.display.cabl=2 \
+    sdm.debug.disable_skip_validate=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.enable_fb_ubwc=1
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.protected_contents=true \
-ro.surface_flinger.set_display_power_timer_ms=10000 \
-ro.surface_flinger.set_touch_timer_ms=5000 \
-ro.surface_flinger.set_idle_timer_ms=9000 \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.set_display_power_timer_ms=10000 \
+    ro.surface_flinger.set_touch_timer_ms=5000 \
+    ro.surface_flinger.set_idle_timer_ms=9000 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
-drm.service.enabled=true
+    drm.service.enabled=true
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.qfp=false
+    persist.qfp=false
 
 # Fm
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.fm.transmitter=false
+    ro.fm.transmitter=false
 
 # Frp
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.frp.pst=/dev/block/bootdevice/by-name/config
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.location \
+    ro.gps.agps_provider=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-av.debug.disable.pers.cache=1 \
-media.aac_51_output_enabled=true \
-media.msm8956hw=0 \
-media.stagefright.audio.sink=280 \
-vendor.mm.enable.qcom_parser=1048575 \
-mm.enable.smoothstreaming=true \
-mmp.enable.3g2=true \
-vendor.audio.hw.aac.encoder=true \
-vendor.vidc.dec.downscalar_height=1088 \
-vendor.vidc.dec.downscalar_width=1920 \
-vendor.vidc.disable.split.mode=1 \
-vendor.vidc.enc.disable.pq=true \
-vendor.vidc.enc.disable_bframes=1 \
-vendor.video.disable.ubwc=1
+    av.debug.disable.pers.cache=1 \
+    media.aac_51_output_enabled=true \
+    media.msm8956hw=0 \
+    media.stagefright.audio.sink=280 \
+    vendor.mm.enable.qcom_parser=1048575 \
+    mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.vidc.dec.downscalar_height=1088 \
+    vendor.vidc.dec.downscalar_width=1920 \
+    vendor.vidc.disable.split.mode=1 \
+    vendor.vidc.enc.disable.pq=true \
+    vendor.vidc.enc.disable_bframes=1 \
+    vendor.video.disable.ubwc=1
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.qti.sys.fw.bservice_enable=true
+    ro.vendor.qti.sys.fw.bservice_enable=true
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.extension_library=libqti-perfd-client.so
+    ro.vendor.extension_library=libqti-perfd-client.so
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.use_data_netmgrd=true \
-persist.data.netmgrd.qos.enable=true \
-persist.vendor.data.mode=concurrent
+    ro.vendor.use_data_netmgrd=true \
+    persist.data.netmgrd.qos.enable=true \
+    persist.vendor.data.mode=concurrent
 
 # Nitz
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.rild.nitz_plmn="" \
-persist.rild.nitz_long_ons_0="" \
-persist.rild.nitz_long_ons_1="" \
-persist.rild.nitz_long_ons_2="" \
-persist.rild.nitz_long_ons_3="" \
-persist.rild.nitz_short_ons_0="" \
-persist.rild.nitz_short_ons_1="" \
-persist.rild.nitz_short_ons_2="" \
-persist.rild.nitz_short_ons_3=""
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
-persist.dbg.volte_avail_ovr=1 \
-persist.dbg.vt_avail_ovr=1 \
-persist.data.iwlan=1 \
-persist.data.iwlan.ipsec.ap=1 \
-persist.dbg.wfc_avail_ovr=1 \
-persist.vendor.radio.apm_sim_not_pwdn=1 \
-persist.radio.multisim.config=dsds \
-persist.vendor.radio.custom_ecc=1 \
-persist.vendor.radio.rat_on=combine \
-persist.vendor.radio.sib16_support=1 \
-persist.vendor.data.iwlan.enable=true \
-ril.subscription.types=NV,RUIM \
-rild.libargs=-d/dev/smd0 \
-rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=22,20 \
-service.qti.ims.enabled=1 \
-telephony.lteOnCdmaDevice=1 \
-persist.radio.VT_ENABLE=1 \
-persist.radio.volte.dan_support=true \
-persist.sys.cust.lte_config=true \
-ro.telephony.iwlan_operation_mode=legacy \
-persist.vendor.vt.supported=1 \
-persist.vendor.data.iwlan.enable=true \
-persist.vendor.sys.cnd.iwlan=1 \
-persist.vendor.cne.logging.qxdm=3974 \
-persist.sys.fflag.override.settings_network_and_internet_v2=true \
-ro.telephony.use_old_mnc_mcc_format=true
+    DEVICE_PROVISIONED=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.data.iwlan=1 \
+    persist.data.iwlan.ipsec.ap=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.data.iwlan.enable=true \
+    ril.subscription.types=NV,RUIM \
+    rild.libargs=-d/dev/smd0 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.default_network=22,20 \
+    service.qti.ims.enabled=1 \
+    telephony.lteOnCdmaDevice=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.volte.dan_support=true \
+    persist.sys.cust.lte_config=true \
+    ro.telephony.iwlan_operation_mode=legacy \
+    persist.vendor.vt.supported=1 \
+    persist.vendor.data.iwlan.enable=true \
+    persist.vendor.sys.cnd.iwlan=1 \
+    persist.vendor.cne.logging.qxdm=3974 \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
+    ro.telephony.use_old_mnc_mcc_format=true
 
 # RCS
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.rcs.supported=1
+    persist.rcs.supported=1
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
+    persist.timed.enable=true
 
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
-net.tcp.2g_init_rwnd=10
+    net.tcp.2g_init_rwnd=10
 
 # UI
 PRODUCT_PROPERTY_OVERRIDES += \
-sys.use_fifo_ui=1
+    sys.use_fifo_ui=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.usb.config.extra=none
+    persist.vendor.usb.config.extra=none
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
-wifi.interface=wlan0
+    wifi.interface=wlan0
