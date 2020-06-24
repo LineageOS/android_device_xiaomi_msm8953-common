86ab68679c2bcec8b9747e690b5e225df1898a9e
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-06-24 18:18:03 +0530

    msm8953-common: Build audio blobs from source
    sound doesn't work with prebuilt blobs on rosy
    
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>
    Change-Id: Ibb42f0d8bd1d9ce0fc11c66b3f3406b281dbb191

--- msm8953.mk
+++ msm8953.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
     libaudioroute \
