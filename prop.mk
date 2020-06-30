d10633c0ab45efc5c64bdb24edf93e741603a762
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-06-30 21:40:12 +0200

    Revert "msm8953-common: props: Enable zygote preforking"
    This reverts commit cbc211b5846ca0dad7541adb3a81efedae1ba25b.
    
    Reason for revert: Don't change default system behavior
    
    Change-Id: I35d50610925a9e18a8b96aa67422feb56c899e88

--- prop.mk
+++ prop.mk
@@ -190,7 +190,3 @@ persist.vendor.usb.config.extra=none
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
-
-# Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.device_config.runtime_native.usap_pool_enabled=true
