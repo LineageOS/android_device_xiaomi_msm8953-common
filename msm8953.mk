ca49f9f94db5fc8eea8bdc70d96bbfda87d2e3e1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-21 17:20:55 +0200

    msm8953-common: Build audio primary HAL from source
    Change-Id: Ie0439cf0be97e16cd9188ba57094fe43a7b363bd

--- msm8953.mk
+++ msm8953.mk
@@ -84,6 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
     libaudioroute \
