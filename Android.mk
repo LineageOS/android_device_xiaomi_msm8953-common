6737bbf0b5938e68e88239519f2a3ffa8f0598d7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-16 11:35:09 +0100

    msm8953-common: Add vince to device guards
    Change-Id: Ibdc235dec0546793d10c1463322520aaf08f1e32

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter mido tissot,$(TARGET_DEVICE)),)
+ifneq ($(filter mido tissot vince,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "mido tissot"
+write_headers "mido tissot vince"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files-qc.txt" true
