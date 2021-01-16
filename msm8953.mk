879eaebe9cfdd94a94966166ec18c5dd794e0d7e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-16 10:17:24 +0100

    msm8953-common: Build custom fingerprint HIDL
    Change-Id: I4ff911c21695acc51de3340e8bd572968ea8e145

--- msm8953.mk
+++ msm8953.mk
@@ -153,6 +153,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
