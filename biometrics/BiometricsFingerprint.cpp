2aaaabc7be2b9d62b122b80ff7b98bb9eb75e303
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-17 03:13:29 +0100

    msm8953-common: biometrics: Add custom fingerprint HAL loading logic
    Change-Id: Ib2529464f1c4e05a7da35cb03ef6a324b469975b

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +36,18 @@ namespace implementation {
 // Supported fingerprint HAL version
 static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
 
+// List of fingerprint HALs
+static std::vector<std::string> splitIntoVector(std::string list) {
+    std::stringstream list_stringstream(list);
+    std::string segment;
+    std::vector<std::string> list_vector;
+    while(std::getline(list_stringstream, segment, ',')) {
+        list_vector.push_back(segment);
+    }
+    return list_vector;
+}
+static const std::vector<std::string> kHALModules = splitIntoVector(HAL_MODULES);
+
 using RequestStatus =
         android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
 
@@ -42,9 +55,14 @@ BiometricsFingerprint *BiometricsFingerprint::sInstance = nullptr;
 
 BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevice(nullptr) {
     sInstance = this; // keep track of the most recent instance
-    mDevice = openHal();
-    if (!mDevice) {
-        ALOGE("Can't open HAL module");
+    for (const auto& HMI : kHALModules) {
+        mDevice = openHal(HMI.c_str());
+        if (!mDevice) {
+            ALOGE("Can't open HAL module, module ID %s", HMI.c_str());
+        } else {
+            ALOGI("Opened fingerprint HAL, module ID %s", HMI.c_str());
+            break;
+        }
     }
 }
 
@@ -211,37 +229,37 @@ IBiometricsFingerprint* BiometricsFingerprint::getInstance() {
     return sInstance;
 }
 
-fingerprint_device_t* BiometricsFingerprint::openHal() {
+fingerprint_device_t* BiometricsFingerprint::openHal(const char *hw_mdl_id) {
     int err;
     const hw_module_t *hw_mdl = nullptr;
     ALOGD("Opening fingerprint hal library...");
-    if (0 != (err = hw_get_module(FINGERPRINT_HARDWARE_MODULE_ID, &hw_mdl))) {
-        ALOGE("Can't open fingerprint HW Module, error: %d", err);
+    if (0 != (err = hw_get_module(hw_mdl_id, &hw_mdl))) {
+        ALOGE("Can't open fingerprint HW Module, module ID %s, error: %d", hw_mdl_id, err);
         return nullptr;
     }
 
     if (hw_mdl == nullptr) {
-        ALOGE("No valid fingerprint module");
+        ALOGE("No valid fingerprint module, module ID %s", hw_mdl_id);
         return nullptr;
     }
 
     fingerprint_module_t const *module =
         reinterpret_cast<const fingerprint_module_t*>(hw_mdl);
     if (module->common.methods->open == nullptr) {
-        ALOGE("No valid open method");
+        ALOGE("No valid open method, module ID %s", hw_mdl_id);
         return nullptr;
     }
 
     hw_device_t *device = nullptr;
 
     if (0 != (err = module->common.methods->open(hw_mdl, nullptr, &device))) {
-        ALOGE("Can't open fingerprint methods, error: %d", err);
+        ALOGE("Can't open fingerprint methods, module ID %s, error: %d", hw_mdl_id, err);
         return nullptr;
     }
 
     if (kVersion != device->version) {
         // enforce version on new devices because of HIDL@2.1 translation layer
-        ALOGE("Wrong fp version. Expected %d, got %d", kVersion, device->version);
+        ALOGE("Wrong fp version, module ID %s. Expected %d, got %d", hw_mdl_id, kVersion, device->version);
         return nullptr;
     }
 
@@ -250,7 +268,7 @@ fingerprint_device_t* BiometricsFingerprint::openHal() {
 
     if (0 != (err =
             fp_device->set_notify(fp_device, BiometricsFingerprint::notify))) {
-        ALOGE("Can't register fingerprint module callback, error: %d", err);
+        ALOGE("Can't register fingerprint module callback, module ID %s, error: %d", hw_mdl_id, err);
         return nullptr;
     }
 
