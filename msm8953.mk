003a5a5ebfd046c5b1ec100d81c2fcc3b0b70a94
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-19 00:30:49 +0100

    msm8953-common: Build some libs from source
    Change-Id: I4a5a7668162575e7424212bc970820dcb26131e3

--- msm8953.mk
+++ msm8953.mk
@@ -87,13 +87,15 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    libaudiopreprocessing \
     libaudioroute \
     libaacwrapper \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    libvolumelistener
+    libvolumelistener \
+    libtinycompress
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
@@ -116,6 +118,10 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
 
 # Camera
 PRODUCT_PACKAGES += \
