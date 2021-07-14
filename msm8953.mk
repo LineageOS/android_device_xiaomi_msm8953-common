4ebd63b4231d008c502e051528d7246d7ca4b4f1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-14 17:42:55 +0200

    msm8953-common: Move QTI permission to vendor repo
    Change-Id: If03de235f0439107996622f8dd2ac39dd8837637

--- msm8953.mk
+++ msm8953.mk
@@ -357,12 +357,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# Whitelisted app
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/permissions/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
