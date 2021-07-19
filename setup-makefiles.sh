c44a5481979220f00b4145ff6b8408a3ae4664de
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 12:27:42 +0200

    msm8953-common: Add support for daisy, sakura and ysl
    Change-Id: I0f59b55abb2585059178905a8e295db48abe29be

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "mido tissot vince"
+write_headers "daisy mido sakura tissot vince ysl"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
