23e784cc9a62920ea99461f5de4d106d05e6d986
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 12:18:13 +0200

    msm8953-common: Remove open source camera components from PRODUCT_PACKAGES
    Change-Id: Ic584eab7dd93abed9ae311f86577f271df77ba86

--- msm8953.mk
+++ msm8953.mk
@@ -111,8 +111,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8953 \
-    libmm-qcamera \
     libstdc++.vendor \
     Snap
 
