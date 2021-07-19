336b43bf767d4d8c0cfdd8d6c89509ec8a06bb20
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 16:07:34 +0200

    msm8953-common: Downrev soundtrigger to 2.1
    * Soundtrigger HAL doesn't really like new HIDL versions
    
    Change-Id: I5503753d4ba35a81e0e84963faeb1deaf1ee2223

--- msm8953.mk
+++ msm8953.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
