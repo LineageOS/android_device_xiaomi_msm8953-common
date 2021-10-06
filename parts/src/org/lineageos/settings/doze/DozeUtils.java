79458128987241c0d04af479cfd6cba652aff145
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:25:13 +0000

    msm8953-common: parts: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to prefernce screen
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Change-Id: I25f6dce7cf202487898facfa66e57ced86a85594

--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -36,6 +36,7 @@ public final class DozeUtils {
 
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
 
+    protected static final String DOZE_ENABLE = "doze_enable";
     protected static final String ALWAYS_ON_DISPLAY = "always_on_display";
 
     protected static final String CATEG_PICKUP_SENSOR = "pickup_sensor";
