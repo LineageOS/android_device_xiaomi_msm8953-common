1f0cd15d75218b5636784e73a35a4f1e909380c6
LuK1337 <priv.luk@gmail.com>
2021-12-15 17:58:52 +0100

    msm8953-common: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.
    
    Change-Id: Iae740ef18505b7f2bb01fe54d45810e72fd521e9

--- msm8953.mk
+++ msm8953.mk
@@ -17,7 +17,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -54,8 +53,7 @@ PRODUCT_COPY_FILES += \
 
 # ANT
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
+    AntHalService
 
 # Device-specific Settings
 PRODUCT_PACKAGES += \
