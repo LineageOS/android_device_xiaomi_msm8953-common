4840f57bb297d3a1876a91d5fbdb5a8ef09f12b3
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:38:45 -0500

    msm8953-common: Specify androidboot.boot_devices
    So that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I270c1899e4487ed5e7657038c4ed07e407476953

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7824900.sdhci
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
