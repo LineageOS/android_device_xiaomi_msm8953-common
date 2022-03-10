ce7e7b2148acb23cd2c71235cb186dad0865a1e7
Georg Veichtlbauer <georg@vware.at>
2022-03-10 16:03:55 +0100

    msm8953-common: Remove CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 ifeq ($(TARGET_KERNEL_VERSION),4.9)
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
-TARGET_KERNEL_CLANG_COMPILE := true
 endif
 
 # ANT
