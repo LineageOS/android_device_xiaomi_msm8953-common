1671a4df2dad19130b9daefcc7687fd1a6459c26
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-13 20:19:49 -0400

    msm8953-common: Move to libhidl shim in hardware/lineage/compat
    Change-Id: Ia2cd305baf3ce0fa7ac52d440a4ff51f94654438

--- msm8953.mk
+++ msm8953.mk
@@ -201,7 +201,8 @@ endif
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libhidltransport \
