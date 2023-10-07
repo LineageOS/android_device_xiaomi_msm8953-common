f72f62fca5f53b74b6504106f2c104588053ccd3
me-cafebabe <me.cafebabe@gmail.com>
2023-10-07 17:26:01 +0800

    msm8953-commom: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7

--- msm8953.mk
+++ msm8953.mk
@@ -371,7 +371,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-service \
+    android.hardware.wifi@1.0-service.legacy \
     libcld80211 \
     libwifi-hal-ctrl \
     libwpa_client \
