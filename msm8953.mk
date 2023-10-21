d04f8e4079e5a0d6e42869eb13fe897c1aa4f0a3
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    msm8953-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- msm8953.mk
+++ msm8953.mk
@@ -311,6 +311,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
