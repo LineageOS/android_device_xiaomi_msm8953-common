ad9c20d432e24e540899c430b0bbacce5ac94fd4
Lafactorial <hknhzl@gmail.com>
2023-11-08 14:58:37 +0300

    msm8953-common : Enable uncompressed apex
    Change-Id: Iee89ea1d83eabed13f4a15b69b990fa22a21759e

--- msm8953.mk
+++ msm8953.mk
@@ -9,6 +9,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
